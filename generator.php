70d14f168eaadbdcfb91b734d2ec832e4b75bb03
Thomas-Duthoit <duthoit.thomas9@gmail.com>
2024-10-12 22:55:22 +0200

    function choisirTileAdjacenteAlea done

--- generator.php
+++ generator.php
@@ -10,13 +10,27 @@
 
 
 
+
+
+
+
+
+
+
+            #################################################################
+            ######                                                   ########
+            ######                  FONCTIONS DEBUG                  ########
+            ######                                                   ########
+            #################################################################
+
+
     function dbg_echo($val) {  // fonction utilisée pour l'affichage en mode DEBUG
         global $DEBUG;  // on récupère la variable $DEBUG qui est extérieure au scope de la fonction
         if (!$DEBUG) return;
         echo $val;
     }
 
-    function dbg_echo_tab($tab) {  // fonction utilisée pour l'affichage en mode DEBUG
+    function dbg_echo_tab($tab) {  // fonction utilisée pour l'affichage de tableaux en mode DEBUG
         global $DEBUG;  // on récupère la variable $DEBUG qui est extérieure au scope de la fonction
         if (!$DEBUG) return;
         echo "<pre>";
@@ -28,6 +42,17 @@
 
 
 
+
+
+
+
+
+
+            ###################################################################
+            ######                                                     ########
+            ######                  VERIFICATIONS GET                  ########
+            ######                                                     ########
+            ###################################################################
 
 
     $DEBUG = false;
@@ -69,7 +94,8 @@
 
     dbg_echo('<h1 style="width:100%;text-align:center">generator.php - mode DEBUG</h1>');
 
-    $infosLab = array(
+    
+    $infosLab = array(                                 // Structure contenant toutes les informations importantes sur le labyrinthe à génrérer
         'height'=> $_GET["height"],                    // largeur du labyrinthe
         'width'=> $_GET["width"],                      // hauteur du labyrinthe
         'nbTiles'=> $_GET["width"] * $_GET["height"],  // nombre de tuiles
@@ -100,20 +126,23 @@
     $labyrinthe = array();
 
     for ($i=0; $i<$infosLab["nbTiles"]; $i++) {
-        $labyrinthe[]=$objTile;
-        $labyrinthe[$i]["valeur"] = $i;
+        $labyrinthe[]=$objTile;          // On clone notre structure
+        $labyrinthe[$i]["valeur"] = $i;  // Et on lui assigne une valeur unique à chaque tile à la création
 
-        if ($i < $infosLab["width"]) {
-            $labyrinthe[$i]["murN"]=0;
+        // Pour la génération du labyrinthe, on désactive les "bordures" du labyrinthe 
+        // pour faciliter les calculs
+
+        if ($i < $infosLab["width"]) {  // Test ...
+            $labyrinthe[$i]["murN"]=0;  // ... et suppression pour la bordure Nord
         }
-        if($i > ($infosLab["nbTiles"]-1)-$infosLab["width"]){
-            $labyrinthe[$i]["murS"]=0;
+        if($i > ($infosLab["nbTiles"]-1)-$infosLab["width"]) {  // Test ...
+            $labyrinthe[$i]["murS"]=0;                          // ... et suppression pour la bordure Sud
         }
-        if($i%$infosLab["width"]==0){
-            $labyrinthe[$i]["murO"]=0;
+        if($i%$infosLab["width"]==0){  // Test ...
+            $labyrinthe[$i]["murO"]=0;  // ... et suppression pour la bordure Ouest
         }
-        if($i%$infosLab["width"]==$infosLab["width"]-1){
-            $labyrinthe[$i]["murE"]=0;
+        if($i%$infosLab["width"]==$infosLab["width"]-1){  // Test ...
+            $labyrinthe[$i]["murE"]=0;                    // ... et suppression pour la bordure Est
         }
     }
 
@@ -122,6 +151,10 @@
     dbg_echo("<hr>");
     
     
+
+
+
+
 
 
 
@@ -146,27 +179,62 @@
     // quand toutes les cases ont le meme id on ouvre des murs exterieurs pour l'entree et la sortie
 
 
-    function choisirTileAdjacenteAlea($tile) {
+    function choisirTileAdjacenteAlea($indice) {
         /**
-         * renvoie l'indice d'une case adjacente à celle passée en paramètre avec un numero
+         * renvoie l'indice d'une case adjacente à celle passée en paramètre avec 
+         * un numero différent de la case actuelle et un mur fermé en commun
+         * renvoie -1 si aucune case ne correspond aux attentes de la fonction
          */
-        //tile a gauche = $tile-1 (murO)
-        //tile a droite = $tile+1 (murE)
-        //tile en haut =  $tile-width (murN)
-        //tile en bas = $tile+width (murS)
-        $tag = $tile-1;
-        $tad = $tile+1;
-        $teh = $tile-$infosLab["width"];
-        $teb = $tile+$infosLab["width"];
+        // tile a gauche = $tile-1 (murO)
+        // tile a droite = $tile+1 (murE)
+        // tile en haut  = $tile-width (murN)
+        // tile en bas   = $tile+width (murS)
+        global $labyrinthe;  // | => On récuupère les variables qui sont en dehors du scope de la fonction
+        global $infosLab;    // |
 
-        $tabTileAdj = [$tag, $tad, $teh, $teb];
-        $chx = $tabTileAdj[array_rand($tabTileAdj)];
-        return $chx;
+        $tile = $labyrinthe[$indice];
+        $tO = $indice-1;                   // tile Ouest
+        $tE = $indice+1;                   // tile Est
+        $tN = $indice-$infosLab["width"];  // tile Nord
+        $tS = $indice+$infosLab["width"];  // tile Sud
+
+        $tabTileAdj = array();                   // Contient tous les indices des cases potentielles adjacentes
+        if ($tile["murN"]) $tabTileAdj[] = $tN;  // Test mur Nord
+        if ($tile["murS"]) $tabTileAdj[] = $tS;  // Test mur Sud
+        if ($tile["murO"]) $tabTileAdj[] = $tO;  // Test mur Ouest
+        if ($tile["murE"]) $tabTileAdj[] = $tE;  // Test mur Est
+
+        if (count($tabTileAdj) == 0) return -1;  // Incompatible: aucun mur fermé
+
+        // Vérification pour savoir si au moins une des tiles adjacentes a une valeur différente à celle passée en paramètre
+        $flag=false;
+        for ($i=0; $i<count($tabTileAdj); $i++) {
+            if ($labyrinthe[$tabTileAdj[$i]]["valeur"] != $tile["valeur"]) {  // test de valeurs différentes
+                $flag = true;
+                break;
+            }
+        }
+        if (!$flag) return -1;  // Incompatible: toutes les tiles adjacentes ont la meme valeur
+
+        do {
+            $chx = $tabTileAdj[rand()%count($tabTileAdj)];         // Choix d'un indice aléatoire ...
+        } while ($labyrinthe[$chx]["valeur"] == $tile["valeur"]);  // .. tant qu'il na pas une valeur différente
+
+        return $chx;  // On renvoie l'indice d'une tile compatible adjacente aléatoire
     }
 
+    dbg_echo('<h2>Génération :</h2>');
     while($infosLab["nbOpenWalls"] != $infosLab["nbOpenWallsTarget"]) {
-        $indice = rand(0, $infosLab["nbTiles"]);
+        do {
+            $indice1 = rand(0, $infosLab["nbTiles"]-1);                   // On récupère un indice aléatoire de tile ...
+        } while (($indice2 = choisirTileAdjacenteAlea($indice1)) == -1);  // ... tant qu'on trouve ne trouve pas au moins une case compatible pour une fusion
+
+        dbg_echo("<h3>Choix de fusion n°" . $infosLab["nbOpenWalls"]+1 . " :</h3>");
+        dbg_echo_tab($labyrinthe[$indice1]);
+        dbg_echo_tab($labyrinthe[$indice2]);
         
+        // TODO: supprimer la ligne (utilisée uniquement pour tester sans rester bloqué dans le while)
+        $infosLab["nbOpenWalls"]++;
     }   
 
 ?>
